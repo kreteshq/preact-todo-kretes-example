c452d8f96e1b31c8a812e403887f5da2f77b46ec
Zaiste <oh@zaiste.net>
2020-08-08 17:01:02 +0200

    define task structure

--- features/Base/View/index.tsx
+++ features/Base/View/index.tsx
@@ -14,14 +14,30 @@ class App extends Component<Props, State> {
     this.setState({ todos: todos.concat(`Item ${todos.length}`) });
   }
   render({ }: Props, { todos = [] }: State) {
-      return (
-      <div>
-        <Header name="ToDos ()" />
-        <ul>
-          {todos.map(todo => <li>{todo}</li>)}
+    return (
+      <div class="task-container">
+        <div class="task-input">
+          <input
+            placeholder="Add new item..."
+            type="text"
+            class="task-new"
+            v-model="state.newTask"
+          />
+          <button class="task-add">Add</button>
+        </div>
+        <ul class="task-list">
+          <li class="task-item" v-for="task in state.tasks">
+            <label class="task-item-container">
+              <div class="task-checkbox">
+                <input type="checkbox" class="opacity-0 absolute" />
+                <svg class="task-checkbox-icon" viewBox="0 0 20 20">
+                  <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+                </svg>
+              </div>
+              <div class="ml-2">test</div>
+            </label>
+          </li>
         </ul>
-        <button onClick={() => this.addTodo()} class="bg-blue-100 border px-2 py-1">Add Todo</button>
-        <Footer>footer content here</Footer>
       </div>
     );
   }
@@ -31,3 +47,5 @@ const Header = ({ name }: any) => <h1 class="text-2xl">{name} List</h1>;
 const Footer = (props: any) => <footer {...props} />;
 
 export { App };
+
+
