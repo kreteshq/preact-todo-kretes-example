1f6f50ecd1c7ff4a0b985c645764d3194dbdac2a
Zaiste <oh@zaiste.net>
2020-08-11 13:57:41 +0200

    longer list of tasks

--- features/Task/Controller/browse.ts
+++ features/Task/Controller/browse.ts
@@ -3,6 +3,7 @@ import { Handler, response } from 'kretes';
 const { OK } = response;
 
 const tasks = [
+  { name: 'Use TypeScript 4.x', done: false },
   { name: 'Learn Kretes', done: false },
   { name: 'Enjoy programming', done: false },
 ]
